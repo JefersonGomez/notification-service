2df4bed10b97cd1090584b59085be10693554289
JefersonGomez <gomesjeje504@gmail.com>
2026-06-05 23:13:35 -0600

    fix dsn para produccion

--- main.go
+++ main.go
@@ -34,21 +34,25 @@ func main() {
 
 	godotenv.Load()
 
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port =%s sslmode=disable",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_PORT"),
-	)
+	// usa DB_URL si existe, si no construye el DSN
+	dsn := os.Getenv("DB_URL")
+	if dsn == "" {
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_USER"),
+			os.Getenv("DB_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("DB_PORT"),
+		)
+	}
+
+	fmt.Println("DSN:", dsn) // log temporal
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-
 	if err != nil {
-		panic("No se pudo conecatar a la base de datos")
-
+		panic("No se pudo conectar a la base de datos")
 	}
-	fmt.Print("Base de datos conectada")
+	fmt.Println("Base de datos conectada")
 
 	controller.DB = db
 
